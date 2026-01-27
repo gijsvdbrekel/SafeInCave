Updated lithostatic pressure and updated dilatancy boundary according to correct formula

examples/mechanics/3_cavern/Plotting/FOS.py
@@ -67,7 +67,7 @@ def q_dil_rd(p_MPa, psi, D1=0.683, D2=0.512, m=0.75, T0=1.5, sigma_ref=1.0):
     denom = (np.sqrt(3.0) * np.cos(psi) - D2 * np.sin(psi))
     denom = np.where(np.abs(denom) < 1e-12, np.sign(denom) * 1e-12, denom)
 
-    sqrtJ2_dil = D1 * ((absI1 / sigma_ref) ** m) / denom + T0
+    sqrtJ2_dil = (D1 * ((absI1 / sigma_ref) ** m) + T0) / denom 
     q_dil = np.sqrt(3.0) * sqrtJ2_dil
     return q_dil
 

examples/mechanics/3_cavern/Plotting/plot_dilatancy.py
@@ -26,7 +26,7 @@ ROOT = r"/home/gvandenbrekel/SafeInCave/OutputNobian"
 
 SELECT = {
     "pressure": "sinus",        # e.g. "sinus", "irregular", "csv_profile", "linear", or None (=all)
-    "caverns": None,            # e.g. ["Regular"] or ["Regular","Irregular"] or None (=all)
+    "caverns": ["Irregular"],            # e.g. ["Regular"] or ["Regular","Irregular"] or None (=all)
     "case_contains": None,      # e.g. "365days" or "8cyc" or "desai_only" or None
 }
 
@@ -448,7 +448,7 @@ def plot_dilatancy_boundaries_all(ax, p_min=0.01, p_max=40.0, npts=500, show_leg
         sgn = np.sign(I1_MPa)
         sgn[sgn == 0.0] = 1.0
         denom = (np.sqrt(3.0) * np.cos(psi_rad) - D2 * np.sin(psi_rad))
-        sqrtJ2_ = D1 * ((I1_MPa / (sgn * sigma_ref)) ** m) / denom + T0
+        sqrtJ2_ = (D1 * ((I1_MPa / (sgn * sigma_ref)) ** m) + T0) / denom 
         return np.sqrt(3.0) * sqrtJ2_
 
     psi_c = -np.pi / 6.0

examples/mechanics/3_cavern/Plotting/plot_stress_state.py
@@ -171,7 +171,7 @@ def plot_dilatancy_boundary(ax,
         sgn = np.sign(I1_MPa)
         sgn[sgn == 0.0] = 1.0
         denom = (np.sqrt(3.0) * np.cos(psi_rad) - D2 * np.sin(psi_rad))
-        sqrtJ2 = D1 * ((I1_MPa / (sgn * sigma_ref)) ** m) / denom + T0
+        sqrtJ2 = (D1 * ((I1_MPa / (sgn * sigma_ref)) ** m) + T0) / denom 
         return np.sqrt(3.0) * sqrtJ2
 
     psi_c = -np.pi/6.0
@@ -361,7 +361,7 @@ def read_stress_paths(case_folder, probes_dict):
     out = {}
     for key, probe_xyz in probes_dict.items():
         idx = post.find_closest_point(probe_xyz, points_p)
-        p = -p_elems[:, idx] / (3.0 * MPA)   # your “/3” correction
+        p = -p_elems[:, idx] / MPA   # Here was the old /3 corrections
         q =  q_elems[:, idx] / MPA
         out[key] = (p, q)
 